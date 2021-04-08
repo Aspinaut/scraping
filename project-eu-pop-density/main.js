e691b91ad90360ed7a07a0554d113230637a556a
Vincent Masse <vincentmasse@protonmail.com>
2021-04-08 12:02:33 +0200

    add nuts0, 2 & 3

--- project-eu-pop-density/main.js
+++ project-eu-pop-density/main.js
@@ -1,65 +1,32 @@
-// import { Loader } from "@googlemaps/js-api-loader"
+const level = 0
+const geojson = require(`./tmp/nuts${level}.json`)
+const density = require(`./tmp/nuts${level}_density.json`)
+const fs = require('fs').promises
 
-const express = require('express')
-const server = express()
-// const html = require('./index.html')
-require('dotenv').config()
+// nuts 2 and 3
 
-// const loader = new Loader({
-//   apiKey: process.env.API_KEY,
-//   version: "weekly",
+// let newMap = geojson.features.map((region) => {
+//   let regionDensity = density.find(el => el["unit,geo\\time"] === region.properties.NUTS_ID)
+//   if (regionDensity) {
+//     if (regionDensity["2019 "]) {
+//       region.properties.density = regionDensity["2019 "]
+//     } else if (regionDensity["2018 "]) {
+//       region.properties.density = regionDensity["2018 "]
+//     }
+//   }
+//   region.properties.events = 0
+//   return region
 // })
-// loader.load().then(() => {
-  // map = new google.maps.Map(document.getElementById("map"), {
-  //   center: { lat: -34.397, lng: 150.644 },
-  //   zoom: 8,
-  // })
-// })
 
-server.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Simple Map</title>
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-        <style type="text/css">
-          /* Always set the map height explicitly to define the size of the div
-           * element that contains the map. */
-          #map {
-            height: 100%;
-          }
+// nuts 0 (countries)
 
-          /* Optional: Makes the sample page fill the window. */
-          html,
-          body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-          }
-        </style>
-        <script>
-          let map;
-
-          function initMap() {
-            map = new google.maps.Map(document.getElementById("map"), {
-              center: { lat: -340.397, lng: 1500.644 },
-              zoom: 8,
-            });
-          }
-        </script>
-      </head>
-      <body>
-        <div id="map"></div>
-
-        <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
-        <script
-          src="https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}&callback=initMap&libraries=&v=weekly"
-          async
-        ></script>
-      </body>
-    </html>
-
-  `)
+let newMap = geojson.features.map((country) => {
+  let countryDensity = density.find(el => el["NUTS_0"] === country.properties.postal)
+  if (countryDensity) {
+    country.properties.density = countryDensity["2020"]
+    country.properties.events = 0
+  }
+  return country
 })
 
-server.listen(3000)
+fs.writeFile(`./tmp/nuts${level}_final.json`, JSON.stringify(newMap))
