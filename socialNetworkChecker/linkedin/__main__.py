4fbdb920dfeed27e07b9733bd763deb1454ff589
Vincent Masse <vincentmasse@protonmail.com>
2021-07-22 13:15:57 +0200

    b2b : add basic JSON read/write

--- socialNetworkChecker/linkedin/__main__.py
+++ socialNetworkChecker/linkedin/__main__.py
@@ -1,21 +1,40 @@
 import dotenv
 from linkedin_api import Linkedin
 import os
-# replace following JSON by any df
-import "./fakeCompanies.json" as companies
+import json
 
 dotenv.load_dotenv()
 
 api = Linkedin(os.getenv('LN_EMAIL'), os.getenv('LN_PASSWORD'))
 
 def check_company(lnCompany):
-    
-    return false
+    # WIP
+    # voir avec farid pour savoir a quoi ressemble le format de ladresse
+    # print(lnCompany["url"])
+    # print(lnCompany["confirmedLocations"][0]["city"]) 
+    # print(lnCompany["headquarter"])
+    return True
 
 def main():
-    for company of companies:
-        lnCompany = api.get_company('culturius')
-        if check_company(lnCompany):
-            # ecrire une nouvelle ligne dans le JSON pour la certifier
+    f = open('fakeCompanies.json', "r+")
+    companies = json.load(f)
+    f.close()
+    f = open('newFakeCompanies.json', "w")
+    f.write('[')
+    for company in companies:
+        try:
+            lnCompany = api.get_company(company["name"])
+            print('Company found on Linkedin ! Let\'s check it...')
+            if check_company(lnCompany):
+                # ecrire une nouvelle ligne dans le JSON pour la certifier
+                rawLine = {"isOnLinkedin": True}
+                company.update(rawLine)
+                json.dump(company, f, indent=4)
+                f.write(',')
+        except KeyError:
+            print('Company not found on Linkedin...')
+    f.write(']')
+    f.close()
+
 
 main()
